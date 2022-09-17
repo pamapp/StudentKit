d9a6973e35b3be92f8cdd4e5caab45964db20fa6
pamapp <alineus2001@yandex.ru>
2022-09-17 10:02:39 +0300

    Remove storyboards

--- StudentKit/SceneDelegate.swift
+++ StudentKit/SceneDelegate.swift
@@ -16,12 +16,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let home = TabBar()
-        self.window?.rootViewController = home
-        window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+        let viewController = TabBar()
+        if let windowScene = scene as? UIWindowScene {
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+            
+        }
+//        
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let home = TabBar()
+//        self.window?.rootViewController = home
+//        window?.makeKeyAndVisible()
+//        window?.windowScene = windowScene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
