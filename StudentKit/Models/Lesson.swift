2f9a6e2885eff2fd4b41a64fcf42e7542b37e86b
pamapp <alineus2001@yandex.ru>
2022-09-24 10:29:49 +0300

    Fix func to get current lesson

--- StudentKit/Models/Lesson.swift
+++ StudentKit/Models/Lesson.swift
@@ -15,16 +15,65 @@ struct Lesson: Codable, Equatable {
     var name: String
     var type: String
     var audience: String
+    
+    var timeStart: Date {
+        let time: String = self.time
+        let date: String = self.date
+        
+        let yearString = getStringDate(format: "yyyy-MM")
+        let timeSubstring = String(time.dropLast(6))
+        
+        let indexDay = date.index(date.startIndex, offsetBy: 2) // get day
+        let dateSubstring = date[..<indexDay]
+        
+        let fullDateString = yearString + "-" + dateSubstring + "T" + timeSubstring + ":00+0000"
+        
+        return convertStringToDate(dateStr: fullDateString)
+    }
+    
+    var timeEnd: Date {
+        let time: String = self.time
+        let date: String = self.date
+        
+        let yearString = getStringDate(format: "yyyy-MM")
+        let timeSubstring = String(time.dropFirst(6))
 
+        let indexDay = date.index(date.startIndex, offsetBy: 2) //get day
+        let dateSubstring = date[..<indexDay]
+        
+        let fullDateString = yearString + "-" + dateSubstring + "T" + timeSubstring + ":00+0000"
+
+        return convertStringToDate(dateStr: fullDateString)
+    }
+    
     static func == (lhs: Lesson, rhs: Lesson) -> Bool {
         return lhs.time == rhs.time
+    }
+
+    func convertStringToDate(dateStr: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return dateFormatter.date(from: dateStr)!
+    }
+    
+    func dateDay() -> String {
+        let indexDay = self.date.index(self.date.startIndex, offsetBy: 2) // get day
+        return String(self.date[..<indexDay])
+    }
+    
+    func getStringDate(format: String) -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: date)
     }
 }
 
 struct LessonsPerDayDataSource {
     
     // MARK: - Private variables -
-    
+
     private var listOfLessons: [[Lesson]]
     
     // MARK: - Initializer -
@@ -92,37 +141,34 @@ struct LessonsPerDayDataSource {
     
     func lessonByIndexPath(for indexPath: IndexPath) -> [Lesson] { listOfLessons[indexPath.section] }
     func headerTitle(_ section: Int) -> String { listOfLessons[section][0].date}
-    
-    func dateToDate(lesson: Lesson) {
-        var fullDateString: String = ""
-        
-        var time: String = lesson.time
-        var date: String = lesson.date
 
-        let currentDate = Date()
+    func getIndexList() -> [Lesson] {
+        var indexForArray: Int = 0
+        let date = Date()
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM"
-        let yearString = dateFormatter.string(from: currentDate)
+        dateFormatter.dateFormat = "dd"
         
-        let index = time.index(time.startIndex, offsetBy: 5) // get start time
-        let timeSubstring = time[..<index]
+        for index in 0..<listOfLessons.count {
+            if listOfLessons[index][0].dateDay() == dateFormatter.string(from: date) {
+                indexForArray = index
+            }
+        }
+        return listOfLessons[indexForArray]
+    }
+    
+    func currentLesson() -> Lesson {
+        var dayLessons: [Lesson]
+        var currentLesson : Lesson = listOfLessons[0][0]
+        let currentDate = Date.now.adding(hours: 3)
         
-        let index2 = date.index(date.startIndex, offsetBy: 2) // get day
-        let dateSubstring = date[..<index2]
+        dayLessons = getIndexList()
         
-        fullDateString = yearString + "-" + dateSubstring + "T" + timeSubstring + ":00+0000"
+        for element in dayLessons {
+            if element.timeStart <= currentDate && element.timeEnd >= currentDate {
+                currentLesson = element
+            }
+        }
         
-        let dateFormatter2 = DateFormatter()
-        dateFormatter2.locale = Locale(identifier: "ru_RU")
-        dateFormatter2.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        
-        let date3 = dateFormatter2.date(from: fullDateString)!
-        
-        print(fullDateString)
-        print(date3)
-        print(currentDate)
-        print(currentDate > date3)
-        
-        print(" ")
+        return currentLesson
     }
 }
